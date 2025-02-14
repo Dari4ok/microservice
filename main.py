90fa394ba79e492537f464189545179ebf3e9513
Dari4ok <darkhanb05@gmail.com>
2025-02-14 18:47:53 +0500

    v0.0.2

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pymongo import MongoClient
 from bson import ObjectId  
-from config import DATABASE_URL, DB_NAME, COLLECTION_NAME, MAIN_SERVER_URL
+from config import DATABASE_URL, DB_NAME, COLLECTION_NAME
+from fastapi.staticfiles import StaticFiles
+import uuid
 import requests
 
 app = FastAPI()
@@ -13,14 +15,20 @@ client = MongoClient(DATABASE_URL)
 db = client[DB_NAME]
 payments = db[COLLECTION_NAME]
 
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.post("/pay")
-async def show_payment_page(request: Request, data: dict = Body(...)):
-    order_id = data.get("order_id")
-    amount = data.get("amount", 0.0)
+async def show_payment_page(
+    request: Request,
+    order_id: str = Form(...),
+    amount: float = Form(...)
+):
     return templates.TemplateResponse(
         "payment_form.html",
         {"request": request, "order_id": order_id, "amount": amount}
     )
+
 
 @app.post("/process_payment")
 def process_payment(
@@ -30,31 +38,26 @@ def process_payment(
     expiry: str = Form(...),
     cvv: str = Form(...)
 ):
+    payment_id = str(uuid.uuid4())
     
     payment_data = {
+        "_id": payment_id,
         "order_id": order_id,
         "amount": amount,
         "status": True  
     }
     payments.insert_one(payment_data)
     
-    requests.post(MAIN_SERVER_URL, json={   
-        "order_id": order_id,
-        "status": True
-    })
-    
-    return RedirectResponse(url=f"/success/{order_id}", status_code=303)
+    return RedirectResponse(url=f"/success/{payment_id}", status_code=303)
 
 @app.get("/success/{payment_id}")
 def payment_success(request: Request, payment_id: str):
-    try:
-        payment = payments.find_one({"_id": ObjectId(payment_id)})
-    except Exception:
-        return templates.TemplateResponse("payment_failed.html", {"request": request})
     
+    payment = payments.find_one({"_id": payment_id})
+
     if not payment:
-        return templates.TemplateResponse("payment_failed.html", {"request": request})
-    
+        return templates.TemplateResponse("payment_failed.html", {"request": request}) 
+
     return templates.TemplateResponse(
         "payment_success.html",
         {
@@ -64,3 +67,10 @@ def payment_success(request: Request, payment_id: str):
             "status": payment["status"],
         }
     )
+
+from fastapi.responses import RedirectResponse
+
+@app.get("/")
+def redirect_to_main_site():
+    main_site_url = "localhost:9078" 
+    return RedirectResponse(url=main_site_url)
